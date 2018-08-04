31f9025de8fff8b9b08a7f012284e9adeaba9e65
Yakir Kadkoda <kadkoda@gmail.com>
2018-08-04 21:29:39 +0300

    Main page addition, logic and UI adjustments

--- client/main/main.js
+++ client/main/main.js
@@ -3,6 +3,8 @@ import angular from 'angular';
 import controller from './main.controller';
 import template from './main.html';
 
+import './main.less';
+
 angular.module('workouts.controllers')
     .config($stateProvider => {
       $stateProvider
