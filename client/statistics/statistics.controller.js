82b6c7ffb70fe2170e6ab07adf33a2907706c007
Shirley Michaely <shirley.michaely@gmail.com>
2018-08-07 19:06:36 +0300

    compilation error fix

--- client/statistics/statistics.controller.js
+++ client/statistics/statistics.controller.js
@@ -85,6 +85,7 @@ angular.module('workouts.controllers').controller(STATISTICS_CONTROLLER, ($scope
         var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
 
         // add the SVG element
+        var svg = d3.select("#workouts-by-gender-graph").append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
