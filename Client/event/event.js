68062a446a68f362e7c07a812b0a0f53953fe206
Yakir Kadkoda <kadkoda@gmail.com>
2018-08-07 23:24:05 +0300

    Added font-face

--- Client/event/event.js
+++ Client/event/event.js
@@ -2,7 +2,7 @@ import angular from 'angular';
 
 import controller from './event.controller';
 import template from './event.html';
-
+import './event.less';
 const MODULE_NAME = 'workouts.controllers';
 
 angular.module(MODULE_NAME)
